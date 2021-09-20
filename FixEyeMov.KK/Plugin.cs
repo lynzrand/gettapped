1995a585303d58c732d406672a6e5f931c687ae2
Rynco Maekawa <lynzrand@outlook.com>
2021-09-21 00:08:46 +0800

    feat(FixEyeMov): add the ability to follow mouse position in editor scene

--- FixEyeMov.KK/Plugin.cs
+++ FixEyeMov.KK/Plugin.cs
@@ -5,14 +5,15 @@ using BepInEx;
 using HarmonyLib;
 using Karenia.FixEyeMov.Core;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Karenia.FixEyeMov.Com3d2
 {
     [BepInPlugin(id, projectName, version)]
     public class Plugin : BaseUnityPlugin
     {
-        public const string id = "cc.karenia.fixeyemov.com3d2";
-        public const string projectName = "FixationalEyeMovements.COM3D2";
+        public const string id = "cc.karenia.fixeyemov.kk";
+        public const string projectName = "FixationalEyeMovements.KK";
         public const string version = "0.1.0";
 
         public Plugin()
@@ -31,7 +32,7 @@ namespace Karenia.FixEyeMov.Com3d2
             Logger = BepInEx.Logging.Logger.CreateLogSource("FixEyeMov");
             var harmony = new Harmony(id);
 
-            harmony.PatchAll(typeof(EyeMovementHook));
+            EyeMovementHook.SetupHooks(harmony);
         }
 
         public static Plugin Instance { get; private set; }
@@ -41,17 +42,81 @@ namespace Karenia.FixEyeMov.Com3d2
 
     public static class EyeMovementHook
     {
+        public static void SetupHooks(Harmony harmony)
+        {
+            harmony.PatchAll(typeof(EyeMovementHook));
+            SceneManager.activeSceneChanged += (Scene curr, Scene next) =>
+            {
+                // Enable mouse look when customizing characters
+                canLookAtMouse = next.name.Contains("CustomScene");
+            };
+        }
+
         private static readonly Dictionary<EyeLookCalc, EyeMovementState> states = new Dictionary<EyeLookCalc, EyeMovementState>();
+        public static bool canLookAtMouse = false;
+        public static bool isLookingAtMouse = false;
+
+        [HarmonyPatch(typeof(ChaControl), "ChangeLookEyesTarget")]
+        [HarmonyPostfix]
+        public static void HookAddLookAtMouse(ChaControl __instance, int targetType)
+        {
+            if (canLookAtMouse)
+            {
+                // Is looking at camera
+                if (targetType == 0 && Camera.main && __instance.objEyesLookTarget && __instance.objEyesLookTargetP)
+                {
+                    // objEyesLookTarget is the real target; objEyesLookTargetP is its parent
+                    __instance.objEyesLookTargetP.transform.SetParent(Camera.main.transform);
+                    isLookingAtMouse = true;
+                    __instance.objEyesLookTarget.transform.localPosition = new Vector3(0, 0, 0);
+                }
+                else
+                {
+                    // reset transform
+                    if (__instance.objEyesLookTargetP.transform.parent == Camera.main.transform)
+                    {
+                        var referenceInfo2 = __instance.GetReferenceInfo(ChaReference.RefObjKey.HeadParent);
+                        if (__instance.objEyesLookTargetP && referenceInfo2)
+                        {
+                            __instance.objEyesLookTargetP.transform.SetParent(referenceInfo2.transform, false);
+                            __instance.objEyesLookTarget.transform.SetParent(__instance.objEyesLookTargetP.transform, false);
+                        }
+                    }
+                    isLookingAtMouse = false;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(EyeLookController), "LateUpdate")]
+        [HarmonyPostfix]
+        public static void HookLookingAtMouse(EyeLookController __instance)
+        {
+            if (isLookingAtMouse)
+            {
+                var mousePos = Input.mousePosition;
+                var isInsideSceenSpace = mousePos.x >= 0 && mousePos.y >= 0 &&
+                    mousePos.x < Screen.width && mousePos.y < Screen.height;
+
+                if (isInsideSceenSpace)
+                {
+                    mousePos.Scale(new Vector3(1f / Screen.width, 1f / Screen.height, 0));
+                    mousePos -= new Vector3(.5f, .5f, 0);
+                    var mat = Camera.main.cameraToWorldMatrix;
+                    var mouseCameraOffset = mat.MultiplyPoint(mousePos);
+                    __instance.target.localPosition = mouseCameraOffset;
+                }
+                else
+                {
+                    __instance.target.localPosition = Vector3.zero;
+                }
+            }
+        }
 
         [HarmonyPatch(typeof(EyeLookCalc), "EyeUpdateCalc")]
         [HarmonyPrefix]
         public static bool AddPoiMove(EyeLookCalc __instance, Vector3 target,
             bool ___initEnd, int ptnNo)
         {
-            return true;
-
-            // Not implemented yet
-
             // Initial calculations, same as source
             if (!___initEnd || !EyeLookCalc.isEnabled || Time.deltaTime == 0f)
             {
@@ -65,11 +130,11 @@ namespace Karenia.FixEyeMov.Com3d2
             EYE_LOOK_TYPE lookType = state.lookType;
 
             // Static eyes
-            if (lookType == EYE_LOOK_TYPE.NO_LOOK)
+            if (lookType == EYE_LOOK_TYPE.TARGET)
             {
             }
 
-            return false;
+            return true;
         }
 
         private static readonly MethodInfo AngleHRateCalc = AccessTools.Method(typeof(EyeLookCalc), "AngleHRateCalc");
--- FixEyeMov.KKSS/Plugin.cs
+++ FixEyeMov.KKSS/Plugin.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using Karenia.FixEyeMov.Core;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Karenia.FixEyeMov.KKSS
 {
@@ -34,27 +37,173 @@ namespace Karenia.FixEyeMov.KKSS
             Logger = BepInEx.Logging.Logger.CreateLogSource("FixEyeMov");
             var harmony = new Harmony(id);
 
-            harmony.PatchAll(typeof(EyeMovementHook));
+            BindHookConfig(Config);
+
+            EyeMovementHook.SetupHooks(harmony);
         }
+
+        internal ConfigEntry<bool> followMouseInEditor;
+        internal ConfigEntry<bool> followMouseInTalkScene;
+        internal ConfigEntry<bool> followMouse;
+        internal ConfigEntry<bool> neckFollowMouse;
 
         public static Plugin Instance { get; private set; }
         public EyeMovementConfig EyeConfig { get; private set; }
         public new BepInEx.Logging.ManualLogSource Logger { get; private set; }
+
+        public void BindHookConfig(ConfigFile config)
+        {
+            followMouse = config.Bind<bool>("MouseTrackng", "EyesFollowMouse", false);
+            neckFollowMouse = config.Bind<bool>("MouseTracking", "NeckFollowMouse", false);
+            followMouseInEditor = config.Bind<bool>("MouseTracking", "FollowMouseInEditor", true);
+            followMouseInTalkScene = config.Bind<bool>("MouseTracking", "FollowMouseInTalkScene", false);
+
+            config.SettingChanged += (f, v) =>
+            {
+                EyeMovementHook.shouldEyesFollowMouse = followMouse.Value;
+                EyeMovementHook.shouldNeckFollowMouse = neckFollowMouse.Value;
+            };
+        }
     }
 
     public static class EyeMovementHook
     {
         private static readonly Dictionary<EyeLookCalc, EyeMovementState> states = new Dictionary<EyeLookCalc, EyeMovementState>();
 
+        public static void SetupHooks(Harmony harmony)
+        {
+            harmony.PatchAll(typeof(EyeMovementHook));
+            SceneManager.activeSceneChanged += (Scene curr, Scene next) =>
+            {
+                // Enable mouse look when customizing characters
+                sceneCanFollowMouse = false;
+                if (Plugin.Instance.followMouseInEditor.Value)
+                {
+                    sceneCanFollowMouse |= next.name.Contains("CustomScene");
+                }
+                if (Plugin.Instance.followMouseInTalkScene.Value)
+                {
+                    sceneCanFollowMouse |= false;
+                }
+                //Plugin.Instance.Logger.LogInfo($"Set CanLookAtMouse = {canLookAtMouse}");
+            };
+        }
+
+        public static bool sceneCanFollowMouse = false;
+
+        public static bool shouldEyesFollowMouse = false;
+        public static bool shouldNeckFollowMouse = false;
+
+        public static bool eyesFollowingMouse = false;
+        public static bool neckFollowingMouse = false;
+
+        [HarmonyPatch(typeof(ChaControl), "ChangeLookEyesTarget")]
+        [HarmonyPostfix]
+        public static void HookChangeEyesLookTarget(ChaControl __instance, int targetType)
+        {
+            if (sceneCanFollowMouse && shouldEyesFollowMouse)
+            {
+                // Is looking at camera
+                if (targetType == 0 && Camera.main && __instance.objEyesLookTarget && __instance.objEyesLookTargetP)
+                {
+                    // we dont need to reset target because we directly sets its position
+                    eyesFollowingMouse = true;
+                    __instance.eyeLookCtrl.target = __instance.objEyesLookTarget.transform;
+                }
+                else
+                {
+                    eyesFollowingMouse = false;
+                }
+                //Plugin.Instance.Logger.LogInfo($"Set isLookingAtMouse = {isLookingAtMouse}");
+            }
+        }
+
+        [HarmonyPatch(typeof(ChaControl), "ChangeLookNeckTarget")]
+        [HarmonyPostfix]
+        public static void HookChangeNeckLookTarget(ChaControl __instance, int targetType)
+        {
+            if (sceneCanFollowMouse && shouldNeckFollowMouse)
+            {
+                // Is looking at camera
+                if (targetType == 0 && Camera.main && __instance.objNeckLookTarget && __instance.objNeckLookTargetP)
+                {
+                    // we dont need to reset target because we directly sets its position
+                    neckFollowingMouse = true;
+                    __instance.neckLookCtrl.target = __instance.objNeckLookTarget.transform;
+                }
+                else
+                {
+                    neckFollowingMouse = false;
+                }
+                //Plugin.Instance.Logger.LogInfo($"Set isLookingAtMouse = {isLookingAtMouse}");
+            }
+        }
+
+        [HarmonyPatch(typeof(EyeLookController), "LateUpdate")]
+        [HarmonyPrefix]
+        public static void HookEyesLookingAtMouse(EyeLookController __instance)
+        {
+            if (eyesFollowingMouse && shouldEyesFollowMouse)
+            {
+                var mousePos = Input.mousePosition;
+                var isMouseInsideScreenSpace = mousePos.x >= 0 && mousePos.y >= 0 &&
+                    mousePos.x < Screen.width && mousePos.y < Screen.height;
+
+                if (isMouseInsideScreenSpace)
+                {
+                    var cam = Camera.main;
+
+                    var monitorPlaneDistance = (cam.transform.position - __instance.transform.position).magnitude / 1.5f;
+
+                    mousePos.z = monitorPlaneDistance;
+                    var rayHit = cam.ScreenToWorldPoint(mousePos);
+
+                    __instance.target.position = rayHit;
+
+                    //Plugin.Instance.Logger.LogInfo($"Looking at mouse @ {rayHit}");
+                }
+                else
+                {
+                    __instance.target.position = Camera.main.transform.position;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(NeckLookControllerVer2), "LateUpdate")]
+        [HarmonyPrefix]
+        public static void HookNeckLookingAtMouse(NeckLookControllerVer2 __instance)
+        {
+            if (neckFollowingMouse && shouldNeckFollowMouse)
+            {
+                var mousePos = Input.mousePosition;
+                var isMouseInsideScreenSpace = mousePos.x >= 0 && mousePos.y >= 0 &&
+                    mousePos.x < Screen.width && mousePos.y < Screen.height;
+
+                if (isMouseInsideScreenSpace)
+                {
+                    var cam = Camera.main;
+
+                    var monitorPlaneDistance = (cam.transform.position - __instance.transform.position).magnitude / 1.5f;
+
+                    mousePos.z = monitorPlaneDistance;
+                    var rayHit = cam.ScreenToWorldPoint(mousePos);
+
+                    __instance.target.position = rayHit;
+
+                    //Plugin.Instance.Logger.LogInfo($"Looking at mouse @ {rayHit}");
+                }
+                else
+                {
+                    __instance.target.position = Camera.main.transform.position;
+                }
+            }
+        }
+
         [HarmonyPatch(typeof(EyeLookCalc), "EyeUpdateCalc")]
         [HarmonyPrefix]
         public static bool AddPoiMove(EyeLookCalc __instance, Vector3 target,
             bool ___initEnd, int ptnNo)
         {
-            return true;
-
-            // Not implemented yet
-
             // Initial calculations, same as source
             if (!___initEnd || !EyeLookCalc.isEnabled || Time.deltaTime == 0f)
             {
@@ -67,12 +216,58 @@ namespace Karenia.FixEyeMov.KKSS
             EyeTypeState state = __instance.eyeTypeStates[ptnNo];
             EYE_LOOK_TYPE lookType = state.lookType;
 
-            // Static eyes
-            if (lookType == EYE_LOOK_TYPE.NO_LOOK)
-            {
-            }
+            //// Static eyes
+            //if (lookType == EYE_LOOK_TYPE.TARGET)
+            //{
+            //}
 
-            return false;
+            return true;
+        }
+
+        //[HarmonyPatch(typeof(EyeLookCalc), "EyeUpdateCalc")]
+        //[HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> TranspileEyeLookCalc(IEnumerable<CodeInstruction> code)
+        {
+            var eyeTypeStates = AccessTools.Field(typeof(EyeLookCalc), "eyeTypeStates");
+            var hAngleLimit = AccessTools.Field(typeof(EyeTypeState), "hAngleLimt");
+            var vAngleLimit = AccessTools.Field(typeof(EyeTypeState), "vAngleLimt");
+
+            /*
+             * 185	0214	ldarg.0
+             * 186	0215	ldfld	class EyeTypeState[] EyeLookCalc::eyeTypeStates
+             * 187	021A	ldarg.2
+             * 188	021B	ldelem.ref
+             * 189	021C	ldfld	float32 EyeTypeState::hAngleLimit
+             * 190	0221	bgt.s	198 (0234) ldc.i4.3
+             * 191	0223	ldloc.s	V_5 (5)
+             * 192	0225	ldarg.0
+             * 193	0226	ldfld	class EyeTypeState[] EyeLookCalc::eyeTypeStates
+             * 194	022B	ldarg.2
+             * 195	022C	ldelem.ref
+             * 196	022D	ldfld	float32 EyeTypeState::vAngleLimit
+             * 197	0232	ble.un.s	200 (0236) ldloc.2
+             * -------------
+             * 198	0234	ldc.i4.
+             * 199	0235	stloc.2
+             * -------------
+             * 200	0236	ldloc.2
+
+             */
+
+            return new CodeMatcher(code)
+                .MatchForward(true, new[]
+                {
+                    new CodeMatch(OpCodes.Ldarg_0),
+                    new CodeMatch(OpCodes.Ldfld,eyeTypeStates),
+                    new CodeMatch(OpCodes.Ldarg_2),
+                    new CodeMatch(OpCodes.Ldelem_Ref),
+                    new CodeMatch(OpCodes.Ldfld,vAngleLimit),
+                    new CodeMatch(OpCodes.Ble_Un_S),
+                })
+                .Advance(1)
+                .SetAndAdvance(OpCodes.Nop, null)
+                .SetAndAdvance(OpCodes.Nop, null)
+                .InstructionEnumeration();
         }
 
         private static readonly MethodInfo AngleHRateCalc = AccessTools.Method(typeof(EyeLookCalc), "AngleHRateCalc");
